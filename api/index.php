2852436446de29c250874a61a9823a3d574d84e2
abim <abimfh@gmail.com>
2025-07-07 20:29:20 +0700

    Configure Vercel with api/index.php workaround

---
+++ api/index.php
@@ -0,0 +1,6 @@
+<?php
+
+// This file acts as a bridge for Vercel's Serverless Function to Laravel's public/index.php
+// Make sure your .env variables are set correctly in Vercel.
+
+require __DIR__ . '/../public/index.php';
